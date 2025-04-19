5ac5c466ba476815a8d191fbe7601a27f5c0662f
jlabcloud <jlabcloud@gmail.com>
2025-04-18 20:10:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-        angular.module('jlabsApp', [])
-            .controller('DropdownController', function() {
-                this.menus = {};
-                this.toggleMenu = function(menuId) {
-                    this.menus[menuId] = !this.menus[menuId];
-                };
-                this.isMenuOpen = function(menuId) {
-                    return !!this.menus[menuId];
-                };
-            })
-            .controller('SearchController', function() {
-                this.query = '';
-                this.handleKeyPress = function(event) {
-                    if (event.key === 'Enter') {
-                        console.log('Search query:', this.query);
-                        // Add your search logic here
-                    }
-                };
-            });
+angular.module('jlabsApp', [])
+.controller('DropdownController', function() {
+    this.menus = {};
+    this.toggleMenu = function(menuId) {
+        this.menus[menuId] = !this.menus[menuId];
+    };
+    this.isMenuOpen = function(menuId) {
+        return !!this.menus[menuId];
+    };
+})
+.controller('SearchController', function() {
+    this.query = '';
+    this.handleKeyPress = function(event) {
+        if (event.key === 'Enter') {
+            console.log('Search query:', this.query);
+            // Add your search logic here
+        }
+    };
+});
