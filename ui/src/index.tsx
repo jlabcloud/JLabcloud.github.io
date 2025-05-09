30fb8e4eabc9cee025d6c724bafc342cc3dbfabb
jlabcloud <jlabcloud@gmail.com>
2025-05-09 14:31:41 -0400

    add Strelka ui and change link icon img src

---
+++ ui/src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { QueryProvider } from './providers/QueryProvider'
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
+  <QueryProvider>
+    <App />
+  </QueryProvider>,
+)
